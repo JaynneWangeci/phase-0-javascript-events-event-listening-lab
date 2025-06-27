1604d18679e5d95102ec67f223d6282865b55593
Jaynne Wangeci <jaynne127@gmail.com>
2025-06-27 21:02:51 +0300

    init

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// index.js
+
 function addingEventListener() {
+  // Get the input element
+  const input = document.getElementById('button');
+
+  // Add click event listener
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
 }
+
+// Call the function to activate the event listener
+addingEventListener();
+
